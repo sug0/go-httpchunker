deb6d906434c96b3b0886a920cac4820f50ead04
sugoiuguu <sugoiuguu@tfwno.gf>
2019-10-15 00:39:54 +0100

    new example

---
+++ examples/byterange.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "os"
+    "log"
+    "fmt"
+
+    "github.com/sug0/go-httpchunker"
+    "github.com/sug0/go-httpchunker/byterange"
+)
+
+func main() {
+    p := byterange.ChunkProvider{
+        URL: os.Args[1],
+        ChunkSize: 2*1024*1024,
+    }
+
+    d := httpchunker.NewDownloader()
+    d.WithLogger(log.New(os.Stderr, "byterange: ", log.LstdFlags))
+
+    errs := d.Download(24, p, httpchunker.Filename{"part_", "out"})
+    if errs != nil {
+        panic(joinErrors(errs))
+    }
+}
+
+func joinErrors(errs []error) error {
+    err := errs[0]
+    for i := 1; i < len(errs); i++ {
+        err = fmt.Errorf("«%w», followed by «%w»", errs[i], err)
+    }
+    return err
+}
