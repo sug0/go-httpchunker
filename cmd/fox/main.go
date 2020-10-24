ab6a8c8e14b52e874bc4a142891ffb57656dbfdc
Tiago Carvalho <sugoiuguu@tfwno.gf>
2020-10-24 02:11:07 +0100

    mangafox downloader

---
+++ cmd/fox/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "os"
+    "log"
+    "fmt"
+
+    "github.com/sug0/go-httpchunker"
+    "github.com/sug0/go-httpchunker/mangafox"
+)
+
+func main() {
+    if len(os.Args) < 2 {
+        fmt.Println("Error: Provide a base url, e.g. http://m.fanfox.net/manga/immortal_regis/v01/c001/")
+        os.Exit(1)
+    }
+
+    p := mangafox.ChunkProvider{BaseURL: os.Args[1]}
+
+    d := httpchunker.NewDownloader()
+    d.WithLogger(log.New(os.Stderr, os.Args[0]+": ", log.LstdFlags))
+
+    errs := d.Download(24, p, httpchunker.Filename{"part_", "out"})
+    if errs != nil {
+        panic(joinErrors(errs))
+    }
+}
+
+func joinErrors(errs []error) error {
+    err := errs[0]
+    for i := 1; i < len(errs); i++ {
+        err = fmt.Errorf("%s\n%s", err, errs[i])
+    }
+    return fmt.Errorf("%s\n", err)
+}
---
+++ mangafox/mangafox.go
@@ -0,0 +1,83 @@
+package mangafox
+
+import (
+    "io"
+    "fmt"
+    "time"
+    "bytes"
+    "regexp"
+    "net/http"
+    "crypto/tls"
+
+    "github.com/sug0/go-httpchunker"
+)
+
+var reg = regexp.MustCompile(`<img src="([^"]+)" style="margin:0 auto;" id="image" />`)
+
+var client = http.Client{
+    Transport: &http.Transport{
+        MaxIdleConnsPerHost: 1024,
+        TLSHandshakeTimeout: 0 * time.Second,
+        TLSClientConfig: &tls.Config{
+            InsecureSkipVerify: true,
+        },
+    },
+}
+
+type ChunkProvider struct {
+    // The base URL without the trailing page number,
+    // e.g. http://m.fanfox.net/manga/immortal_regis/v01/c001/
+    BaseURL string
+}
+
+func (p ChunkProvider) ChunkStream() (<-chan httpchunker.Chunk, error) {
+    if p.BaseURL[len(p.BaseURL)-1] == '/' {
+        // truncate slash
+        p.BaseURL = p.BaseURL[:len(p.BaseURL)-1]
+    }
+    ch := make(chan httpchunker.Chunk, 24)
+    go func() {
+        defer close(ch)
+        var buf bytes.Buffer
+        for page := 1; ; page++ {
+            comicPageUrl := fmt.Sprintf("%s/%d.html", p.BaseURL, page)
+            rsp, err := client.Get(comicPageUrl)
+            if err != nil || rsp.StatusCode != 200 {
+                return
+            }
+            func() { // use a new function to close body
+                defer rsp.Body.Close()
+                buf.Reset()
+                io.Copy(&buf, rsp.Body)
+            }()
+            imageUrl := parseUrl(buf.Bytes())
+            if imageUrl == nil {
+                return
+            }
+            ch <- httpchunker.NewChunk("GET", string(imageUrl), nil)
+        }
+    }()
+    return ch, nil
+}
+
+func parseUrl(p []byte) []byte {
+    for {
+        line := lineGet(p)
+        if line == nil {
+            return nil
+        }
+        sub := reg.FindSubmatch(line)
+        if sub != nil {
+            return append([]byte("https:"), sub[1]...)
+        }
+        p = p[len(line)+1:]
+    }
+}
+
+func lineGet(p []byte) []byte {
+    i := bytes.IndexByte(p, '\n')
+    if i == -1 {
+        return nil
+    }
+    return p[:i]
+}
